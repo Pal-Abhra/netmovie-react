b12166c46baa61d4b88f0367cb52f31c5b3a5716
Abhra pal <palabhra1612@gmail.com>
2023-09-13 00:09:39 +0530

    homepage- hero banner section done css remaining

--- src/App.jsx
+++ src/App.jsx
@@ -18,26 +18,34 @@ function App() {
   const {url} = useSelector((state) => state.home)
   console.log(url);
   useEffect(() => {
-    apiTesting()
+    fetchApiConfig()
     
   }, []);
 
   const api_key = import.meta.env.VITE_APP_TMDB_TOKEN
   
-  const apiTesting = () => {
-    fetchDataFromApi(`/movie/popular?api_key=${api_key}`)
+  const fetchApiConfig = () => {
+    fetchDataFromApi(`/configuration?api_key=${api_key}`)
       
       //correct this.. api key is visible
       .then((res) => {
         console.log(res);
-        dispatch(getApiConfiguration(res));
+
+        const url = {
+          backdrop: res.images.secure_base_url + "original",
+          poster: res.images.secure_base_url + "original",
+          profile: res.images.secure_base_url + "original",
+
+        };
+          
+        dispatch(getApiConfiguration(url));
       })
   }
 
   return (
     
     <BrowserRouter>
-      <Header />
+      {/* <Header /> */}
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path=":mediaType/:id" element={<Details />} />
@@ -45,7 +53,7 @@ function App() {
         <Route path='/explore/:mediaType' element={<Explore />} />
         <Route path='*' element={<PageNotFound />} />
       </Routes>
-      <Footer/>
+      {/* <Footer/> */}
     </BrowserRouter>
   )
 }
--- src/pages/home/heroBanner/HeroBanner.jsx
+++ src/pages/home/heroBanner/HeroBanner.jsx
@@ -1,8 +1,57 @@
-import React from 'react'
+import { useState, useEffect } from 'react'
+import { useNavigate } from "react-router-dom"
+import {useSelector} from "react-redux"
 import "./style.scss"
+import useFetch from '../../../hooks/useFetch'
+import Img from "../../../components/lazyLoadImage/Img"
+import ContentWrapper from "../../../components/contentWrapper/ContentWrapper"
+
 const HeroBanner = () => {
+  const api_key = import.meta.env.VITE_APP_TMDB_TOKEN
+  const [background, setBackground] = useState("");
+  const [query, setquery] = useState("");
+  const navigate = useNavigate();
+  const {url} = useSelector((state)=>state.home)
+  const { data, loading } = useFetch(`/movie/upcoming?api_key=${api_key}`);
+  useEffect(() => {
+    const bg = url.backdrop + data?.results?.[Math.floor(Math.random(0) * 20)]?.backdrop_path
+    setBackground(bg);
+  }, [data])
+
+  const searchQueryHandler = (event) => {
+    if (event.key === "Enter" && query.length > 0) {
+      navigate(`/search/${query}`);
+    }
+  }
+
+
   return (
-    <div>HeroBanner</div>
+    <div className='heroBanner'>
+      {!loading && <div className='backdrop-img'>
+        <Img src={background}/>
+      </div>}
+
+      <div className='opacity-layer'>
+
+      </div>
+
+      <ContentWrapper>
+        <div className='heroBannerContent'>
+          <span className='title'>WELCOME</span>
+          <span className='subTitle'>Millons of movies, TV shows and people to discover, Explore now.</span>
+          <div className='searchInput'>
+            <input
+              type='text'
+              placeholder='Search for a movie or TV show'
+              onChange={(e) => setquery(e.target.value)}
+              onKeyUp={searchQueryHandler}
+            />
+            <button>Search</button>
+          </div>
+        </div>
+      </ContentWrapper>
+     
+    </div>
   )
 }
 
