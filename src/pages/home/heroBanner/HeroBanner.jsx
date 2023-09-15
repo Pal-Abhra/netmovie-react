305ca82335cbe25cd1bc67b591bad14b23dad568
Abhra pal <palabhra1612@gmail.com>
2023-09-15 23:52:19 +0530

    homepage css is done

--- src/pages/home/heroBanner/HeroBanner.jsx
+++ src/pages/home/heroBanner/HeroBanner.jsx
@@ -1,48 +1,51 @@
-import { useState, useEffect } from 'react'
-import { useNavigate } from "react-router-dom"
-import {useSelector} from "react-redux"
-import "./style.scss"
-import useFetch from '../../../hooks/useFetch'
-import Img from "../../../components/lazyLoadImage/Img"
-import ContentWrapper from "../../../components/contentWrapper/ContentWrapper"
+import { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { useSelector } from "react-redux";
+import "./style.scss";
+import useFetch from "../../../hooks/useFetch";
+import Img from "../../../components/lazyLoadImage/Img";
+import ContentWrapper from "../../../components/contentWrapper/ContentWrapper";
 
 const HeroBanner = () => {
-  const api_key = import.meta.env.VITE_APP_TMDB_TOKEN
+  const api_key = import.meta.env.VITE_APP_TMDB_TOKEN;
   const [background, setBackground] = useState("");
   const [query, setquery] = useState("");
   const navigate = useNavigate();
-  const {url} = useSelector((state)=>state.home)
+  const { url } = useSelector((state) => state.home);
   const { data, loading } = useFetch(`/movie/upcoming?api_key=${api_key}`);
   useEffect(() => {
-    const bg = url.backdrop + data?.results?.[Math.floor(Math.random(0) * 20)]?.backdrop_path
+    const bg =
+      url.backdrop +
+      data?.results?.[Math.floor(Math.random(0) * 20)]?.backdrop_path;
     setBackground(bg);
-  }, [data])
+  }, [data]);
 
   const searchQueryHandler = (event) => {
     if (event.key === "Enter" && query.length > 0) {
       navigate(`/search/${query}`);
     }
-  }
-
+  };
 
   return (
-    <div className='heroBanner'>
-      {!loading && <div className='backdrop-img'>
-        <Img src={background}/>
-      </div>}
+    <div className="heroBanner">
+      {!loading && (
+        <div className="backdrop-img">
+          <Img src={background} />
+        </div>
+      )}
 
-      <div className='opacity-layer'>
-
-      </div>
+      <div className="opacity-layer"></div>
 
       <ContentWrapper>
-        <div className='heroBannerContent'>
-          <span className='title'>WELCOME</span>
-          <span className='subTitle'>Millons of movies, TV shows and people to discover, Explore now.</span>
-          <div className='searchInput'>
+        <div className="heroBannerContent">
+          <span className="title">WELCOME</span>
+          <span className="subTitle">
+            Millons of movies, TV shows and people to discover, Explore now.
+          </span>
+          <div className="searchInput">
             <input
-              type='text'
-              placeholder='Search for a movie or TV show'
+              type="text"
+              placeholder="Search for a movie or TV show"
               onChange={(e) => setquery(e.target.value)}
               onKeyUp={searchQueryHandler}
             />
@@ -50,9 +53,8 @@ const HeroBanner = () => {
           </div>
         </div>
       </ContentWrapper>
-     
     </div>
-  )
-}
+  );
+};
 
-export default HeroBanner
+export default HeroBanner;
