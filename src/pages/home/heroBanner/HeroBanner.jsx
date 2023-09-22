3f6838707d53fbed38b7b8214d0b0ffd6799c777
Abhra pal <palabhra1612@gmail.com>
2023-09-22 15:31:22 +0530

    minor changes

--- src/pages/home/heroBanner/HeroBanner.jsx
+++ src/pages/home/heroBanner/HeroBanner.jsx
@@ -5,19 +5,21 @@ import "./style.scss";
 import useFetch from "../../../hooks/useFetch";
 import Img from "../../../components/lazyLoadImage/Img";
 import ContentWrapper from "../../../components/contentWrapper/ContentWrapper";
+import Typewriter from "typewriter-effect";
 
 const HeroBanner = () => {
   const api_key = import.meta.env.VITE_APP_TMDB_TOKEN;
   const [background, setBackground] = useState("");
+  const [movieName, setMovieName] = useState("");
   const [query, setquery] = useState("");
   const navigate = useNavigate();
   const { url } = useSelector((state) => state.home);
   const { data, loading } = useFetch(`/movie/upcoming?api_key=${api_key}`);
   useEffect(() => {
-    const bg =
-      url.backdrop +
-      data?.results?.[Math.floor(Math.random(0) * 20)]?.backdrop_path;
+    const movie = data?.results?.[Math.floor(Math.random(0) * 20)];
+    const bg = url.backdrop + movie?.backdrop_path;
     setBackground(bg);
+    setMovieName(movie?.title);
   }, [data]);
 
   const searchQueryHandler = (event) => {
@@ -38,7 +40,16 @@ const HeroBanner = () => {
 
       <ContentWrapper>
         <div className="heroBannerContent">
-          <span className="title">WELCOME</span>
+          <span className="title">
+            <Typewriter
+              options={{
+                strings: ["WELCOME", movieName],
+                autoStart: true,
+                loop: true,
+                cursor: "",
+              }}
+            />
+          </span>
           <span className="subTitle">
             Millons of movies, TV shows and people to discover, Explore now.
           </span>
