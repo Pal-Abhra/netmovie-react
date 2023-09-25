93e37b058f3b9f713a499160b9706ba7431557fa
Abhra pal <palabhra1612@gmail.com>
2023-09-25 23:06:57 +0530

    Detail Section is partially done

--- src/pages/details/Details.jsx
+++ src/pages/details/Details.jsx
@@ -1,9 +1,29 @@
-import React from 'react'
-import "./style.scss"
+import React from "react";
+import "./style.scss";
+import DetailsBanner from "./detailsBanner/DetailsBanner";
+import { useParams } from "react-router-dom";
+import useFetch from "../../hooks/useFetch";
+import Cast from "./cast/Cast";
+import VideosSection from "./videosSection/VideosSection";
+
 function Details() {
+  const api_key = import.meta.env.VITE_APP_TMDB_TOKEN;
+  const { mediaType, id } = useParams();
+  const { data, loading } = useFetch(
+    `/${mediaType}/${id}/videos?api_key=${api_key}`
+  );
+  const { data: credits, loading: creditsLoading } = useFetch(
+    `/${mediaType}/${id}/credits?api_key=${api_key}`
+  );
+
   return (
-    <div>Details</div>
-  )
+    <div>
+      <DetailsBanner video={data?.results?.[0]} crew={credits?.crew} />
+      <Cast data={credits?.cast} loading={creditsLoading} />
+      <VideosSection data={data} loading={loading} />{" "}
+      {/* Video Section has some issues */}
+    </div>
+  );
 }
 
-export default Details
+export default Details;
