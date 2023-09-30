6e7231e6b505b43b210bbb168088c17e25a2d852
Abhra pal <palabhra1612@gmail.com>
2023-09-30 22:32:00 +0530

    minor change

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import Home from "./pages/home/Home";
 import Details from "./pages/details/Details";
 import SearchResult from "./pages/searchResult/SearchResult";
 import Explore from "./pages/explore/Explore";
-import PageNotFound from "./pages/404/pageNoteFound";
+import PageNotFound from "./pages/404/PageNoteFound";
 import Footer from "./components/footer/footer";
 import Header from "./components/header/Header";
 
